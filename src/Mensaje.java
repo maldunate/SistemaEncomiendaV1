b023844f9eb69ee0994dc722aecb067817cb66d3
Ignacio <ijtoresano@uc.cl>
2015-10-12 15:09:08 -0300

    -

--- src/Mensaje.java
+++ src/Mensaje.java
@@ -1,4 +1,32 @@
 
 public class Mensaje {
+	
+	public String texto;
+	public Sucursal destinatario;
+	public Sucursal origen;
+	
+	public Mensaje(String texto, Sucursal destinatario, Sucursal origen) {
+		super();
+		this.texto = texto;
+		this.destinatario = destinatario;
+		this.origen = origen;
+	}
+
+	public Sucursal getDestinatario() {
+		return destinatario;
+	}
+
+	public void setDestinatario(Sucursal destinatario) {
+		this.destinatario = destinatario;
+	}
+
+	public Sucursal getOrigen() {
+		return origen;
+	}
+
+	public void setOrigen(Sucursal origen) {
+		this.origen = origen;
+	}
+	
 
 }
--- src/Sucursal.java
+++ src/Sucursal.java
@@ -9,7 +9,7 @@ public class Sucursal {
 	Cajero cajero;
 	ArrayList<Camion> listaCamiones;
 	ArrayList<Encomienda> listaEncomiendas;
-	
+	ArrayList<Mensaje> listaMensajes;
 	
 	/**
 	 * @param id
@@ -18,10 +18,11 @@ public class Sucursal {
 	 * @param operador
 	 * @param cajero
 	 * @param listaCamiones
+	 * @param listaMensajes 
 	 */
 	
 	public Sucursal(int id, String direccion, String nombre, Operador operador, Cajero cajero,
-			ArrayList<Camion> listaCamiones) {
+			ArrayList<Camion> listaCamiones, ArrayList<Mensaje> listaMensajes) {
 		super();
 		this.id = id;
 		this.direccion = direccion;
@@ -29,11 +30,8 @@ public class Sucursal {
 		this.operador = operador;
 		this.cajero = cajero;
 		this.listaCamiones = listaCamiones;
+		this.listaMensajes = listaMensajes;
 	}
 
-
-	public void enviarMensaje(){
-		
-	}
 
 }
